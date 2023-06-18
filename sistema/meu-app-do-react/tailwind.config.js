e3df3613c37d8c8050cfa35100f9c113826ca0e3
GuilhermeVolpato <volpato2103@gmail.com>
2023-06-18 19:29:26 -0300

    feat: CRUD em tempo real com formulário para inserir e update, lista de items vem direto do banco de dados com o select e é possivel deletar com onLongPress no item

--- sistema/meu-app-do-react/tailwind.config.js
+++ sistema/meu-app-do-react/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '435px': '435px',
+      }
+    },
   },
   plugins: [],
 }
